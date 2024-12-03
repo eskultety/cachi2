eskultety/cachi2

commit ba75c2b39f0bfc2493837dd0e06596aa32af8a79

gomod: Automatically detect vendoring
Everytime there's a vendor dir commited to the repository, we want to
check its integrity and avoid the prefetch of dependencies. This is
currently the behavior of the gomod-vendor-check flag.

This commit implements the automatic detection and checking of the
vendored content, making both the gomod-vendor and the
gomod-vendor-check flag effectively 'noop'. They are kept for backwards
compatibility reasons.

Both flags could previously be used to perfom vendoring on a repository
that did not have a 'vendor' folder checked-in. This is no longer the
case, and all vendoring now needs to be provided by the user.

Signed-off-by: Bruno Pimentel <bpimente@redhat.com>

cachi2/core/package_managers/gomod.py
@@ -888,9 +888,7 @@ def _resolve_gomod(
 
     # Vendor dependencies if the gomod-vendor flag is set
     flags = request.flags
-    should_vendor, can_make_changes = _should_vendor_deps(
-        flags, app_dir, config.gomod_strict_vendor
-    )
+    should_vendor = app_dir.join_within_root("vendor").path.is_dir()
     if should_vendor:
         if go_work_path and go_work_path.join_within_root("vendor").path.is_dir():
             # NOTE: the same error will be reported even for 1.21 which doesn't support workspace
@@ -898,7 +896,7 @@ def _resolve_gomod(
             # in the foreseeable future, a not so user friendly error should be fine
             raise UnsupportedFeature("Go workspace vendoring is not supported")
 
-        downloaded_modules = _vendor_deps(go, app_dir, can_make_changes, run_params)
+        downloaded_modules = _vendor_deps(go, app_dir, run_params)
     else:
         log.info("Downloading the gomod dependencies")
         downloaded_modules = (
@@ -1149,46 +1147,6 @@ class GoCacheTemporaryDirectory(tempfile.TemporaryDirectory[str]):
             Go()(["clean", "-modcache"], {"env": {"GOPATH": self.name, "GOCACHE": self.name}})
         finally:
             super().__exit__(exc, value, tb)
-
-
-def _should_vendor_deps(
-    flags: Iterable[str], app_dir: RootedPath, strict: bool
-) -> Tuple[bool, bool]:
-    """
-    Determine if Cachi2 should vendor dependencies and if it is allowed to make changes.
-
-    This is based on the presence of flags:
-    - gomod-vendor-check => should vendor, can only make changes if vendor dir does not exist
-    - gomod-vendor => should vendor, can make changes
-
-    :param flags: flags from the Cachi2 request
-    :param app_dir: absolute path to the app directory
-    :param strict: fail the request if the vendor dir is present but the flags are not used?
-    :return: (should vendor: bool, allowed to make changes in the vendor directory: bool)
-    :raise PackageRejected: if the vendor dir is present, the flags are not used and we are strict
-    """
-    vendor = app_dir.join_within_root("vendor").path
-
-    if "gomod-vendor-check" in flags:
-        return True, not vendor.exists()
-    if "gomod-vendor" in flags:
-        return True, True
-
-    if strict and vendor.is_dir():
-        raise PackageRejected(
-            reason=(
-                'The "gomod-vendor" or "gomod-vendor-check" flag must be set when your repository '
-                "has vendored dependencies."
-            ),
-            solution=(
-                "Consider removing the vendor/ directory and letting Cachi2 download dependencies "
-                "instead.\n"
-                "If you do want to keep using vendoring, please pass one of the required flags."
-            ),
-            docs=VENDORING_DOC,
-        )
-
-    return False, False
 
 
 class ModuleVersionResolver:
@@ -1559,7 +1517,6 @@ def _parse_vendor(module_dir: RootedPath) -> Iterable[ParsedModule]:
 def _vendor_deps(
     go: Go,
     app_dir: RootedPath,
-    can_make_changes: bool,
     run_params: dict[str, Any],
 ) -> Iterable[ParsedModule]:
     """
@@ -1577,7 +1534,7 @@ def _vendor_deps(
     """
     log.info("Vendoring the gomod dependencies")
     go(["mod", "vendor"], run_params)
-    if not can_make_changes and _vendor_changed(app_dir):
+    if _vendor_changed(app_dir):
         raise PackageRejected(
             reason=(
                 "The content of the vendor directory is not consistent with go.mod. "

cachi2/interface/cli.py
@@ -166,6 +166,7 @@ def fetch_deps(
         False,
         "--gomod-vendor",
         help=(
+            "DEPRECATED (no longer has any effect when set). "
             "Fetch go deps via 'go mod vendor' rather than 'go mod download'. If you "
             "have a vendor/ dir, one of --gomod-vendor/--gomod-vendor-check is required."
         ),
@@ -174,6 +175,7 @@ def fetch_deps(
         False,
         "--gomod-vendor-check",
         help=(
+            "DEPRECATED (no longer has any effect when set). "
             "Same as gomod-vendor, but will not make unexpected changes if you "
             "already have a vendor/ directory (will fail if changes would be made)."
         ),

tests/integration/test_data/gomod_vendor_check_no_vendor/.build-config.yaml
@@ -1,10 +1,0 @@
-environment_variables:
-- name: GOCACHE
-  value: ${output_dir}/deps/gomod
-- name: GOMODCACHE
-  value: ${output_dir}/deps/gomod/pkg/mod
-- name: GOPATH
-  value: ${output_dir}/deps/gomod
-- name: GOPROXY
-  value: file://${GOMODCACHE}/cache/download
-project_files: []

tests/integration/test_data/gomod_vendored_with_flag/.build-config.yaml
@@ -1,10 +1,0 @@
-environment_variables:
-- name: GOCACHE
-  value: ${output_dir}/deps/gomod
-- name: GOMODCACHE
-  value: ${output_dir}/deps/gomod/pkg/mod
-- name: GOPATH
-  value: ${output_dir}/deps/gomod
-- name: GOPROXY
-  value: file://${GOMODCACHE}/cache/download
-project_files: []

tests/integration/test_gomod.py
@@ -36,46 +36,6 @@ log = logging.getLogger(__name__)
             ),
             id="gomod_without_deps",
         ),
-        pytest.param(
-            utils.TestParameters(
-                repo="https://github.com/cachito-testing/gomod-vendored.git",
-                ref="ff1960095dd158d3d2a4f31d15b244c24930248b",
-                packages=({"path": ".", "type": "gomod"},),
-                check_output=False,
-                check_deps_checksums=False,
-                check_vendor_checksums=False,
-                expected_exit_code=2,
-                expected_output='The "gomod-vendor" or "gomod-vendor-check" flag'
-                " must be set when your repository has vendored dependencies",
-            ),
-            id="gomod_vendored_without_flag",
-        ),
-        # Test case checks if vendor folder with dependencies will remain unchanged in cloned
-        # source repo, deps folder in output folder should be empty.
-        pytest.param(
-            utils.TestParameters(
-                repo="https://github.com/cachito-testing/gomod-vendored.git",
-                ref="ff1960095dd158d3d2a4f31d15b244c24930248b",
-                packages=({"path": ".", "type": "gomod"},),
-                flags=["--gomod-vendor"],
-                expected_exit_code=0,
-                expected_output="All dependencies fetched successfully",
-            ),
-            id="gomod_vendored_with_flag",
-        ),
-        # Test case checks if vendor folder will be created with dependencies in cloned
-        # source repo, deps folder in output folder should be empty.
-        pytest.param(
-            utils.TestParameters(
-                repo="https://github.com/cachito-testing/gomod-vendor-check-no-vendor.git",
-                ref="7ba383d5592910edbf7f287d4b5a00c5ababf751",
-                packages=({"path": ".", "type": "gomod"},),
-                flags=["--gomod-vendor-check"],
-                expected_exit_code=0,
-                expected_output="All dependencies fetched successfully",
-            ),
-            id="gomod_vendor_check_no_vendor",
-        ),
         # Test case checks if vendor folder with dependencies will remain unchanged in cloned
         # source repo, deps folder in output folder should be empty.
         pytest.param(

tests/unit/package_managers/test_gomod.py
@@ -43,7 +43,6 @@ from cachi2.core.package_managers.gomod import (
     _process_modules_json_stream,
     _resolve_gomod,
     _setup_go_toolchain,
-    _should_vendor_deps,
     _validate_local_replacements,
     _vendor_changed,
     _vendor_deps,
@@ -290,9 +289,11 @@ def test_resolve_gomod(
 @mock.patch("cachi2.core.package_managers.gomod._get_gomod_version")
 @mock.patch("cachi2.core.package_managers.gomod.ModuleVersionResolver")
 @mock.patch("cachi2.core.package_managers.gomod._validate_local_replacements")
+@mock.patch("cachi2.core.package_managers.gomod._vendor_changed")
 @mock.patch("subprocess.run")
 def test_resolve_gomod_vendor_dependencies(
     mock_run: mock.Mock,
+    mock_vendor_changed: mock.Mock,
     mock_validate_local_replacements: mock.Mock,
     mock_version_resolver: mock.Mock,
     mock_get_gomod_version: mock.Mock,
@@ -339,8 +340,9 @@ def test_resolve_gomod_vendor_dependencies(
     mock_version_resolver.get_golang_version.return_value = "v0.1.0"
     mock_go_release.return_value = "go0.1.0"
     mock_get_gomod_version.return_value = ("0.1.1", "0.1.2")
+    mock_vendor_changed.return_value = False
 
-    flags: list[Flag] = ["gomod-vendor"]
+    flags: list[Flag] = []
     if force_gomod_tidy:
         flags.append("force-gomod-tidy")
 
@@ -373,31 +375,6 @@ def test_resolve_gomod_vendor_dependencies(
     assert list(resolve_result.parsed_modules) == expect_result.parsed_modules
     assert list(resolve_result.parsed_packages) == expect_result.parsed_packages
     assert resolve_result.modules_in_go_sum == expect_result.modules_in_go_sum
-
-
-@mock.patch("cachi2.core.package_managers.gomod._disable_telemetry")
-@mock.patch("cachi2.core.package_managers.gomod.Go.release", new_callable=mock.PropertyMock)
-@mock.patch("cachi2.core.package_managers.gomod._get_gomod_version")
-def test_resolve_gomod_vendor_without_flag(
-    mock_get_gomod_version: mock.Mock,
-    mock_go_release: mock.PropertyMock,
-    mock_disable_telemetry: mock.Mock,
-    tmp_path: Path,
-    gomod_request: Request,
-) -> None:
-    module_dir = gomod_request.source_dir.join_within_root("path/to/module")
-    module_dir.path.joinpath("vendor").mkdir(parents=True)
-    version_resolver = mock.Mock()
-    mock_go_release.return_value = "go0.1.0"
-    mock_get_gomod_version.return_value = ("0.1.1", "0.1.2")
-    mock_disable_telemetry.return_value = None
-
-    expected_error = (
-        'The "gomod-vendor" or "gomod-vendor-check" flag must be set when your repository has '
-        "vendored dependencies."
-    )
-    with pytest.raises(PackageRejected, match=expected_error):
-        _resolve_gomod(module_dir, gomod_request, tmp_path, version_resolver)
 
 
 @pytest.mark.parametrize("force_gomod_tidy", [False, True])
@@ -1393,84 +1370,28 @@ def test_invalid_local_replacements(tmpdir: Path) -> None:
         _validate_local_replacements(modules, app_path)
 
 
-@pytest.mark.parametrize(
-    "flags, vendor_exists, expect_result",
-    [
-        # no flags => should not vendor, cannot modify (irrelevant)
-        ([], True, (False, False)),
-        ([], False, (False, False)),
-        # gomod-vendor => should vendor, can modify
-        (["gomod-vendor"], True, (True, True)),
-        (["gomod-vendor"], False, (True, True)),
-        # gomod-vendor-check, vendor exists => should vendor, cannot modify
-        (["gomod-vendor-check"], True, (True, False)),
-        # gomod-vendor-check, vendor does not exist => should vendor, can modify
-        (["gomod-vendor-check"], False, (True, True)),
-        # both vendor flags => gomod-vendor-check takes priority
-        (["gomod-vendor", "gomod-vendor-check"], True, (True, False)),
-    ],
-)
-def test_should_vendor_deps(
-    flags: list[str],
-    vendor_exists: bool,
-    expect_result: tuple[bool, bool],
-    rooted_tmp_path: RootedPath,
-) -> None:
-    if vendor_exists:
-        rooted_tmp_path.join_within_root("vendor").path.mkdir()
-
-    assert _should_vendor_deps(flags, rooted_tmp_path, False) == expect_result
-
-
-@pytest.mark.parametrize(
-    "flags, vendor_exists, expect_error",
-    [
-        ([], True, True),
-        ([], False, False),
-        (["gomod-vendor"], True, False),
-        (["gomod-vendor-check"], True, False),
-    ],
-)
-def test_should_vendor_deps_strict(
-    flags: list[str], vendor_exists: bool, expect_error: bool, rooted_tmp_path: RootedPath
-) -> None:
-    if vendor_exists:
-        rooted_tmp_path.join_within_root("vendor").path.mkdir()
-
-    if expect_error:
-        msg = 'The "gomod-vendor" or "gomod-vendor-check" flag must be set'
-        with pytest.raises(PackageRejected, match=msg):
-            _should_vendor_deps(flags, rooted_tmp_path, True)
-    else:
-        _should_vendor_deps(flags, rooted_tmp_path, True)
-
-
-@pytest.mark.parametrize("can_make_changes", [True, False])
 @pytest.mark.parametrize("vendor_changed", [True, False])
 @mock.patch("cachi2.core.package_managers.gomod.Go._run")
 @mock.patch("cachi2.core.package_managers.gomod._vendor_changed")
 def test_vendor_deps(
     mock_vendor_changed: mock.Mock,
     mock_run_cmd: mock.Mock,
-    can_make_changes: bool,
     vendor_changed: bool,
     rooted_tmp_path: RootedPath,
 ) -> None:
     app_dir = rooted_tmp_path.join_within_root("some/module")
     run_params = {"cwd": app_dir}
     mock_vendor_changed.return_value = vendor_changed
-    expect_error = vendor_changed and not can_make_changes
 
-    if expect_error:
+    if vendor_changed:
         msg = "The content of the vendor directory is not consistent with go.mod."
         with pytest.raises(PackageRejected, match=msg):
-            _vendor_deps(Go(), app_dir, can_make_changes, run_params)
+            _vendor_deps(Go(), app_dir, run_params)
     else:
-        _vendor_deps(Go(), app_dir, can_make_changes, run_params)
+        _vendor_deps(Go(), app_dir, run_params)
 
     mock_run_cmd.assert_called_once_with(["go", "mod", "vendor"], **run_params)
-    if not can_make_changes:
-        mock_vendor_changed.assert_called_once_with(app_dir)
+    mock_vendor_changed.assert_called_once_with(app_dir)
 
 
 def test_parse_vendor(rooted_tmp_path: RootedPath, data_dir: Path) -> None:

commit f32f306fce153444c5d22a256bfa1c968d434230

package_managers: gomod: Drop redundant can_make_changes doc parameter
The argument in question was semantically dropped already, this is just
a doc string left over.

Fixes: ba75c2b39f0bfc2493837dd0e06596aa32af8a79

Signed-off-by: Erik Skultety <eskultet@redhat.com>

cachi2/core/package_managers/gomod.py
@@ -1535,7 +1535,6 @@ def _vendor_deps(
     contained the correct content.
 
     :param app_dir: path to the module directory
-    :param can_make_changes: is Cachi2 allowed to make changes?
     :param run_params: common params for the subprocess calls to `go`
     :return: the list of Go modules parsed from vendor/modules.txt
     :raise PackageRejected: if vendor directory changed and Cachi2 is not allowed to make changes